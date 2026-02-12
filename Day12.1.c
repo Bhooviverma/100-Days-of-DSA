0d0f7d8e89af93eff07bea1f4983395fe9fa5a99
Bhoovi Verma <bhooviverma02@gmail.com>
2026-02-12 14:48:36 +0530

    Add files via upload

---
+++ Day12.1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    printf("Enter dimensions of 2D matrix: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix[100][100];
+
+    // Input matrix elements
+    printf("Enter elements of the matrix: ");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check if matrix is square
+    if(m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    // Check symmetry
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+
+    return 0;
+}
