8d610a4df80e0af2c6d0598dda6912644a2d8797
Bhoovi Verma <bhooviverma02@gmail.com>
2026-02-17 16:55:07 +0530

    Add files via upload

---
+++ Day17.1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Read size of array
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read array elements
+    printf("Enter elements of array: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Assume first element as max and min
+    int max = arr[0];
+    int min = arr[0];
+
+    // Traverse array
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    // Print result
+    printf("Max: %d\n", max);
+    printf("Min: %d\n", min);
+
+    return 0;
+}
