01331630538139f13ce18b18068102e5ff9b4235
Bhoovi Verma <bhooviverma02@gmail.com>
2026-02-18 23:55:51 +0530

    Add files via upload

---
+++ Day18.2.c
@@ -0,0 +1,21 @@
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    
+    *returnSize = numsSize;
+    
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    
+    // Step 1: Store left products
+    result[0] = 1;
+    for(int i = 1; i < numsSize; i++) {
+        result[i] = result[i - 1] * nums[i - 1];
+    }
+    
+    // Step 2: Multiply with right products
+    int rightProduct = 1;
+    for(int i = numsSize - 1; i >= 0; i--) {
+        result[i] = result[i] * rightProduct;
+        rightProduct *= nums[i];
+    }
+    
+    return result;
+}
