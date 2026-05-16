f8fe800de72824f5ab0259cdfeb1eabb7efda3f8
Bhoovi Verma <bhooviverma02@gmail.com>
2026-05-16 23:11:06 +0530

    Add files via upload

---
+++ Day91.2.c
@@ -0,0 +1,24 @@
+void sortColors(int* nums, int numsSize) {
+    int i = 0, j = 0;
+
+    while (i < numsSize) {
+        if (nums[i] == 0) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            j++;
+        }
+        i++;
+    }
+
+    i = j;
+    while (i < numsSize) {
+        if (nums[i] == 1) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            j++;
+        }
+        i++;
+    }
+}
